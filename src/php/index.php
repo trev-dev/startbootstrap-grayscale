dd2db7b36ad33c2eac647c82289a44aca3ceef56
Trevor Barton <trev@trevdev.ca>
2019-01-17 14:43:20 -0800

    more granular php components

--- src/php/index.php
+++ src/php/index.php
@@ -1,7 +1,7 @@
 <?php
-  include './components/head.php';
-  include './components/nav.php';
-  include './components/body.php';
-  include './components/contact.php';
-  include './components/footer.php';
+  include 'components/head.php';
+  include 'components/nav.php';
+  include 'components/body.php';
+  include 'components/contact.php';
+  include 'components/footer.php';
 ?>
